253a34b0062020da9bf19aff7f4e672310cc7c57
matternstsr <matternstsr@gmail.com>
2023-08-14 10:02:20 -0700

    got help

--- sort.h
+++ sort.h
@@ -23,5 +23,6 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
 /* MAIN Prototypes */
+void selection_sort(int *array, size_t size)
 void bubble_sort(int *array, size_t size);
 #endif /* SORT_H */
