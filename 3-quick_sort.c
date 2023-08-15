d469afda7ba500a4023f0f5b03c7e0f43b262667
matternstsr <matternstsr@gmail.com>
2023-08-14 20:30:22 -0700

    going back to recurrsive that travis helped with eventhough it no workie

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,75 +1,76 @@
 #include "sort.h"
 
 /**
- * partition - Lomuto partition scheme.
- * @arr: Pointer to the array.
- * @lo: The lower index.
- * @hi: The higher index.
- * Return: Pivot index.
- */
-
-int partition(int arr[], int lo, int hi)
-{
-	int pivot = arr[hi];
-	int i = lo - 1;
-	int j = lo;
-	int temp = arr[i];
-
-
-	for (j = lo; j <= hi - 1; j++)
-	{
-		if (arr[j] <= pivot)
-	{
-			i++;
-			temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp;
-		}
-	}
-
-	temp = arr[i + 1];
-	arr[i + 1] = arr[hi];
-	arr[hi] = temp;
-
-	return i + 1;
-}
-
-/**
- * quick_sort - Sort an array of integers in ascending order using quicksort.
- * @array: Pointer to the array.
- * @size: Number of elements in the array.
+ * quick_sort - Sorts an array of integers in ascending order using quicksort.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ * Return: Nothing.
  */
 void quick_sort(int *array, size_t size)
 {
-	int stack[100];
-	int top = -1;
-	int high, low, pi;
+    int low = 0, high = 0;
 
-	if (size <= 1 || array == NULL)
-		return;
+    if (size <= 1 || array == NULL || size == 0)
+        return;
 
-	/* Push initial values of low and high to the stack */
-	stack[++top] = 0;
-	stack[++top] = size - 1;
+    low = 0;
+    high = size - 1;
 
-	while (top >= 0) {
-		high = stack[top--];
-		low = stack[top--];
+    recursive_quick_sort(array, low, high, size);
+}
 
-		/* Perform Lomuto partition */
-		pi = partition(array, low, high);
+/**
+ * lomuto_partition - Lomuto partition scheme for quicksort.
+ * @arr: The array to be partitioned.
+ * @lo: The lower index of the partition.
+ * @hi: The higher index of the partition.
+ * @size: The size of the array.
+ * Return: The partition index.
+ */
+int lomuto_partition(int *arr, int lo, int hi, size_t size)
+{
+    int pivot = 0;
+    int i = 0, j = 0;
+    size_t tmp;
 
-		/* If there are elements on the left side of pivot, push them to stack */
-		if (pi - 1 > low) {
-			stack[++top] = low;
-			stack[++top] = pi - 1;
-		}
+    pivot = arr[hi];
+    i = lo - 1;
 
-		/* If there are elements on the right side of pivot, push them to stack */
-		if (pi + 1 < high) {
-			stack[++top] = pi + 1;
-			stack[++top] = high;
-		}
-	}
+    for (j = lo; j <= hi; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i += 1;
+            if (i != j)
+            {
+                tmp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = tmp;
+                print_array(arr, size);
+            }
+        }
+    }
+    return i;
+}
+
+/**
+ * recursive_quick_sort - Recursive function to perform quicksort.
+ * @arr: The array to be sorted.
+ * @low: The lower index of the subarray.
+ * @high: The higher index of the subarray.
+ * @size: The size of the array.
+ * Return: Nothing.
+ */
+void recursive_quick_sort(int *arr, int low, int high, size_t size)
+{
+    int pivot_index = 0;
+
+    if (low < high)
+    {
+        pivot_index = lomuto_partition(arr, low, high, size);
+
+        recursive_quick_sort(arr, low, pivot_index - 1, size);
+        recursive_quick_sort(arr, pivot_index + 1, high, size);
+    }
 }
 
