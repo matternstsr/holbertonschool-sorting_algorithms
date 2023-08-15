75140af6bb3a15fc20b03cd8fb05a10d2de62ef0
matternstsr <matternstsr@gmail.com>
2023-08-14 20:07:40 -0700

    pushing changes to 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,74 +1,70 @@
 #include "sort.h"
 
 /**
- * quick_sort - Sort an array of integers in sort way
- * @array: pointer
- * @size: size
- * Return: nothing
+ * quick_sort - Sort an array of integers in ascending order using quicksort.
+ * @array: Pointer to the array.
+ * @size: Number of elements in the array.
  */
-
 void quick_sort(int *array, size_t size)
 {
-	int high = stack[top--];
-	int low = stack[top--];
-	int pi;
-	int stack[size];
-	int top = -1;
+    if (size <= 1 || array == NULL)
+        return;
 
-	if (size <= 1 || array == NULL || size == 0)
-		return;
+    int stack[size];
+    int top = -1;
+    
+    /* Push initial values of low and high to the stack*/
+    stack[++top] = 0;
+    stack[++top] = size - 1;
 
-	/* Initialize a stack for iterative quicksort */
-	stack[size];
-	top = -1;
+    while (top >= 0) {
+        int high = stack[top--];
+        int low = stack[top--];
 
-	/* Push initial values of low and high to the stack */
-	stack[++top] = 0;
-	stack[++top] = size - 1;
+        /* Perform Lomuto partition */
+        int pi = partition(array, low, high);
 
-	while (top >= 0) {
-		/* Pop high and low from stack */
-		high = stack[top--];
-		low = stack[top--];
+        /* If there are elements on the left side of pivot, push them to stack */
+        if (pi - 1 > low) {
+            stack[++top] = low;
+            stack[++top] = pi - 1;
+        }
 
-		/* Perform lomuto partition */
-		pi = partition(array, low, high, size);
-
-		/* If there are elements on the left side of pivot, push them to stack */
-		if (pi - 1 > low) {
-			stack[++top] = low;
-			stack[++top] = pi - 1;
-		}
-
-		/* If there are elements on the right side of pivot, push them to stack */
-		if (pi + 1 < high) {
-			stack[++top] = pi + 1;
-			stack[++top] = high;
-		}
-	}
+        /* If there are elements on the right side of pivot, push them to stack */
+        if (pi + 1 < high) {
+            stack[++top] = pi + 1;
+            stack[++top] = high;
+        }
+    }
 }
 
 /**
- * partition - lomuto partition scheme.
- * @arr: pointer.
- * @lo: the lower
- * @hi: the higher
- * Return: pivot index
+ * partition - Lomuto partition scheme.
+ * @arr: Pointer to the array.
+ * @lo: The lower index.
+ * @hi: The higher index.
+ * Return: Pivot index.
  */
-
 int partition(int arr[], int lo, int hi)
 {
-	int pivot = arr[hi];
-	int i = lo - 1;
-	int j = lo;
+    int pivot = arr[hi];
+    int i = lo - 1;
 
-	for (j = lo; j <= hi - 1; j++)
-	{
-		if (arr[j] <= pivot)
-	{
-		i++;
-		quick_sort(&arr[i], &arr[j]);
-	}
-	quick_sort(&arr[i + 1], &arr[hi]);
-	return i + 1;
+    for (int j = lo; j <= hi - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            /* Swap arr[i] and arr[j] */
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    /* Swap arr[i+1] and arr[hi] to place pivot in correct position */
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[hi];
+    arr[hi] = temp;
+
+    return i + 1;
 }
+
